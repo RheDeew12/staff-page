4fc99dd6a4343a5397a0e113236a60d0f8a57530
RheDeew12 <127844922+RheDeew12@users.noreply.github.com>
2024-05-06 22:49:24 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.get("/", (req, res) => {
   
-  res.sendFile(path.join(__dirname, "home_staff.html"));
+  res.sendFile(path.join(__dirname, "home_admin.php"));
 });
 
 const PORT = process.env.PORT || 8080;
